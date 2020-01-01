814ecb0eb707308a50c4b10bd57df3974be0aebe
Daniel Kipkemei <dann.professional@gmail.com>
2020-01-01 17:35:38 +0300

    fearure(schedule): add restaurant schedule - add restaurant schedules feature - add weekdays for schedules - seed restaurants and weekdays

--- restaurants/api/serializers.py
+++ restaurants/api/serializers.py
@@ -1,10 +1,26 @@
-from .models import Restaurant
+from .models import Restaurant, Schedule, Weekday
 from rest_framework import serializers
 
-class RestaurantSerializer(serializers.HyperlinkedModelSerializer):
+class WeekdaySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Weekday
+        fields = ['day', 'iso_weekday']
+
+
+class ScheduleSerializer(serializers.ModelSerializer):
+    weekday = WeekdaySerializer(many=False, read_only=True)
+
+    class Meta:
+        model = Schedule
+        fields = ['opening_time', 'closing_time', 'weekday']
+
+
+class RestaurantSerializer(serializers.ModelSerializer):
+    schedules = ScheduleSerializer(many=True, read_only=True)
+    open = serializers.ReadOnlyField()
     class Meta:
         model = Restaurant
-        fields = ['url', 'name']
+        fields = ['url', 'name', 'open',  'schedules']
         lookup_field = 'slug'
         extra_kwargs = {
             'url': {'lookup_field': 'slug'}
--- restaurants/api/tests.py
+++ restaurants/api/tests.py
@@ -1,9 +1,11 @@
 import json
+import csv
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 from django.urls import reverse
 from .models import Restaurant
 from .serializers import RestaurantSerializer
+from django.conf import settings
 
 
 # initialize the APIClient app
@@ -12,15 +14,19 @@ client = APIClient()
 class RestaurantTest(APITestCase):
     """ Test module for Restaurants """
 
+    base_url = '/restaurants/'
+
     def setUp(self):
-        self.test_restaurant = {'name':'Cool Restaurant'}
-        Restaurant.objects.create(
-                name=self.test_restaurant['name'])
+        self.restaurant_count = 0
+        seed_dir = settings.SEED_DIR
+        with open(seed_dir + '/restaurants.csv') as csv_file:
+            reader = csv.reader(csv_file)
+            for row in reader: self.restaurant_count += 1
 
     def test_get_all_restaurants(self):
         """Test if user can get all restaruants"""
-        response = client.get('/restaurants/')
-        self.assertEqual(response.data['count'], 1)
+        response = client.get(self.base_url)
+        self.assertEqual(response.data['count'], self.restaurant_count)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_restaurant_slug(self):
@@ -30,13 +36,30 @@ class RestaurantTest(APITestCase):
 
     def test_get_one_restaurants(self):
         """Test if user can get all restaruants"""
-        response = client.get('/restaurants/cool-restaurant/')
-        self.assertEqual(response.data['name'], self.test_restaurant['name'])
+        restaurant = client.get(self.base_url)
+        slug=restaurant.data['results'][0]['url'].split('/', 4)[4]
+        response = client.get(self.base_url + slug)
+        self.assertEqual(response.data['name'], restaurant.data['results'][0]['name'])
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
     def test_search_restaurants(self):
         """Test if user can search for restaruants"""
-        response = client.get('/restaurants/?search=cool')
-        self.assertEqual(response.data['results'][0]['name'], self.test_restaurant['name'])
-        self.assertEqual(response.data['count'], 1)
+        response = client.get(self.base_url + '?search=Restaurant')
+        self.assertTrue(response.data['count'] < self.restaurant_count)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_restaurant_schedules(self):
+        """Test if getting restarant includes schedule"""
+        restaurant = client.get(self.base_url)
+        slug=restaurant.data['results'][0]['url'].split('/', 4)[4]
+        response = client.get(self.base_url + slug)
+        self.assertTrue(len(response.data['schedules'])>1)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_restaurant_schedules_weekday(self):
+        """Test if restarant schedule includes weekday"""
+        restaurant = client.get(self.base_url)
+        slug=restaurant.data['results'][0]['url'].split('/', 4)[4]
+        response = client.get(self.base_url + slug)
+        self.assertTrue(response.data['schedules'][0]['weekday'])
         self.assertEqual(response.status_code, status.HTTP_200_OK)
